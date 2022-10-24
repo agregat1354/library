29853e4851017183a8ef032e844ec204b6f90521
agregat1354 <icollectreports@gmail.com>
2022-10-24 20:21:32 +0200

    add Book display function

--- js/script.js
+++ js/script.js
@@ -7,6 +7,29 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+Book.prototype.info = function () {
+  let ret = "";
+  ret += `${this.title} written by ${this.author}, ${this.pages} pages, `;
+  ret += this.read ? "already read." : "not read yet.";
+  return ret;
+};
+
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+function displayBooks() {
+  myLibrary.forEach((elem) => {
+    const currBook = document.createElement("div");
+    currBook.textContent += elem.info();
+    document.body.appendChild(currBook);
+  });
+}
+
+let heriPota = new Book("Harry Potter", "J.K Rowling", 295, false);
+let quo = new Book("Quo Vadis", "Henryk Sienkiewicz", 431, true);
+
+addBookToLibrary(heriPota);
+addBookToLibrary(quo);
+
+displayBooks();
